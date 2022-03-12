5257510ee2311d3f1e1a415233d0b5277e19e6f6
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2022-03-12 16:30:42 +0100

    use exp4j package for expression calculation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,12 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import tk.pratanumandal.expr4j.ExpressionEvaluator;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_1,btn_2,btn_3,btn_4,btn_5,btn_6,btn_7,btn_8,btn_9,btn_0,btn_Add,btn_Sub,btn_Mul,btn_Div,btn_calc,btn_dec,btn_clear;
+    Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_0, btn_Add, btn_Sub, btn_Mul, btn_Div, btn_calc, btn_dec, btn_clear;
     EditText ed1;
     float Value1, Value2;
-    boolean mAddition, mSubtract, mMultiplication, mDivision ;
+    boolean mAddition, mSubtract, mMultiplication, mDivision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,77 +43,77 @@ public class MainActivity extends AppCompatActivity {
         btn_0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"0");
+                ed1.setText(ed1.getText() + "0");
             }
         });
 
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"1");
+                ed1.setText(ed1.getText() + "1");
             }
         });
 
         btn_2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"2");
+                ed1.setText(ed1.getText() + "2");
             }
         });
 
         btn_3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"3");
+                ed1.setText(ed1.getText() + "3");
             }
         });
 
         btn_4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"4");
+                ed1.setText(ed1.getText() + "4");
             }
         });
 
         btn_5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"5");
+                ed1.setText(ed1.getText() + "5");
             }
         });
 
         btn_6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"6");
+                ed1.setText(ed1.getText() + "6");
             }
         });
 
         btn_7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"7");
+                ed1.setText(ed1.getText() + "7");
             }
         });
 
         btn_8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"8");
+                ed1.setText(ed1.getText() + "8");
             }
         });
 
         btn_9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+"9");
+                ed1.setText(ed1.getText() + "9");
             }
         });
 
         btn_dec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ed1.setText(ed1.getText()+".");
+                ed1.setText(ed1.getText() + ".");
             }
         });
 
@@ -119,69 +121,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (ed1 == null){
-                    ed1.setText("");
-                }else {
-                    Value1 = Float.parseFloat(ed1.getText() + "");
-                    mAddition = true;
-                    ed1.setText(null);
-                }
+                ed1.setText(ed1.getText() + "+");
+
             }
         });
 
         btn_Sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Value1 = Float.parseFloat(ed1.getText() + "");
-                mSubtract = true ;
-                ed1.setText(null);
+                ed1.setText(ed1.getText() + "-");
+
             }
         });
 
         btn_Mul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Value1 = Float.parseFloat(ed1.getText() + "");
-                mMultiplication = true ;
-                ed1.setText(null);
+                ed1.setText(ed1.getText() + "*");
             }
         });
 
         btn_Div.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Value1 = Float.parseFloat(ed1.getText()+"");
-                mDivision = true ;
-                ed1.setText(null);
+                ed1.setText(ed1.getText() + "/");
             }
         });
 
         btn_calc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Value2 = Float.parseFloat(ed1.getText() + "");
 
-                if (mAddition == true){
+                String expr = ed1.getText().toString();
+                ExpressionEvaluator exprEval = new ExpressionEvaluator();
+                double result = exprEval.evaluate(expr);
+                ed1.setText(result + "");
 
-                    ed1.setText(Value1 + Value2 +"");
-                    mAddition=false;
-                }
-
-
-                if (mSubtract == true){
-                    ed1.setText(Value1 - Value2 +"");
-                    mSubtract=false;
-                }
-
-                if (mMultiplication == true){
-                    ed1.setText(Value1 * Value2 + "");
-                    mMultiplication=false;
-                }
-
-                if (mDivision == true){
-                    ed1.setText(Value1 / Value2+"");
-                    mDivision=false;
-                }
             }
         });
 
@@ -191,5 +166,6 @@ public class MainActivity extends AppCompatActivity {
                 ed1.setText("");
             }
         });
+
     }
 }
